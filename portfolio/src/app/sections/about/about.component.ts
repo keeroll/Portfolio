52645c6d16a2be9c91dc824c3fec78eec6810cfe
Kyrylo Akatov <kirillakatov@gmail.com>
2025-05-22 22:54:29 +0200

    Skeleton for about section

--- portfolio/src/app/sections/about/about.component.ts
+++ portfolio/src/app/sections/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
+import { ContainerComponent } from '../../shared';
 
 @Component({
   selector: 'app-about',
-  imports: [TranslatePipe],
+  imports: [TranslatePipe, ContainerComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
   standalone: true
