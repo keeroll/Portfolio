794548769551528587e31146d7cbaa6c2d64c631
Kyrylo Akatov <kirillakatov@gmail.com>
2025-07-13 17:59:52 +0200

    Added language selector

--- portfolio/src/app/core/header/header.component.ts
+++ portfolio/src/app/core/header/header.component.ts
@@ -1,16 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { NgIf } from '@angular/common';
+import { NgIf, NgFor, NgClass } from '@angular/common';
+import { TranslationService } from '../../services/translation.service';
+import { Language } from '../../enums/language.enum';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive, NgIf],
+  imports: [RouterLink, RouterLinkActive, NgIf, NgFor, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   standalone: true
 })
 export class HeaderComponent {
   isMobileMenuOpen = false;
+
+  languages = [
+    { code: Language.EN, label: 'EN' },
+    { code: Language.UA, label: 'UA' }
+  ];
+
+  constructor(public translationService: TranslationService) {}
+
+  get currentLang(): string {
+    return this.translationService.currentLang;
+  }
+
+  switchLanguage(lang: string): void {
+    this.translationService.switchLanguage(lang);
+  }
 
   toggleMobileMenu(): void {
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
