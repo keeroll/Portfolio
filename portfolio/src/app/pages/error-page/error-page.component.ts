f9f7e515fb6d2ee029d74d2b8d10219e67d8008c
Kyrylo Akatov <kirillakatov@gmail.com>
2025-07-20 17:07:41 +0200

    Updated error page

--- portfolio/src/app/pages/error-page/error-page.component.ts
+++ portfolio/src/app/pages/error-page/error-page.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
+import { ContainerComponent } from '../../shared/container/container.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-error-page',
-  imports: [TranslatePipe],
+  imports: [TranslatePipe, ContainerComponent],
   templateUrl: './error-page.component.html',
   styleUrl: './error-page.component.scss',
   standalone: true
 })
 export class ErrorPageComponent {
+  constructor(private router: Router) {}
 
+  public goHome(): void {
+    this.router.navigate(['/']);
+  }
+
+  public goBack(): void {
+    window.history.back();
+  }
 }
