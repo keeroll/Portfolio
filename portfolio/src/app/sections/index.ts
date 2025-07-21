fb53a8e9c3ee54efb7a73852831a593b4940c946
Kyrylo Akatov <kirillakatov@gmail.com>
2025-07-22 00:05:21 +0200

    Fix build

--- portfolio/src/app/sections/index.ts
+++ portfolio/src/app/sections/index.ts
@@ -1,4 +1,3 @@
 export { AboutComponent } from './about/about.component';
 export { ProjectsComponent } from './projects/projects.component';
 export { ProjectCardComponent } from './projects/project-card/project-card.component';
-export { ProjectDetailsComponent } from './projects/project-details/project-details.component';
