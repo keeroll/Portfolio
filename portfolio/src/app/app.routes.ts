a771cebbb6e69731d663f38ee3305540febc9b47
Kyrylo Akatov <kirillakatov@gmail.com>
2025-08-03 23:57:06 +0200

    Added separate error path

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -6,5 +6,6 @@ export const routes: Routes = [
     { path: 'projects/:id', component: ProjectDetailsPageComponent },
     { path: 'certificates', component: CertificatesPageComponent },
     { path: 'cv', component: CvPageComponent },
+    { path: 'error', component: ErrorPageComponent },
     { path: '**', component: ErrorPageComponent }
 ];
