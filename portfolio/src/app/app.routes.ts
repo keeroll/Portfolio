10d5d90c4278dc39297f4dd3de3f3763211e063f
Kyrylo Akatov <kirillakatov@gmail.com>
2025-08-03 23:39:03 +0200

    Added 404 html for github

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -6,5 +6,6 @@ export const routes: Routes = [
     { path: 'projects/:id', component: ProjectDetailsPageComponent },
     { path: 'certificates', component: CertificatesPageComponent },
     { path: 'cv', component: CvPageComponent },
+    { path: '404-redirect', component: ErrorPageComponent },
     { path: '**', component: ErrorPageComponent }
 ];
